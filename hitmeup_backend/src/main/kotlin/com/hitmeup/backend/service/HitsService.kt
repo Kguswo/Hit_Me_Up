9561191a0ab6767010a7e6ae822ea52899099a07
김현재 <nowalex322@naver.com>
2025-04-03 05:12:21 +0900

    [#9] fix: redisTemplate 타입 <Any, Any>에서 <String, Long>로 일치하도록 변경

--- hitmeup_backend/src/main/kotlin/com/hitmeup/backend/service/HitsService.kt
+++ hitmeup_backend/src/main/kotlin/com/hitmeup/backend/service/HitsService.kt
@@ -4,7 +4,7 @@ import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
 
 @Service
-class HitsService(private val redisTemplate: RedisTemplate<Any, Any>) {
+class HitsService(private val redisTemplate: RedisTemplate<String, Long>) {
 
     // URL에 대한 방문자 수 증가
     fun incrementHits(url: String): Long {
@@ -15,12 +15,6 @@ class HitsService(private val redisTemplate: RedisTemplate<Any, Any>) {
     // URL에 대한 현재 방문자 수 조회
     fun getHits(url: String): Long {
         val key = "hits:$url"
-        val value = redisTemplate.opsForValue().get(key)
-        return when (value) {
-            null -> 0
-            is Long -> value
-            is Number -> value.toLong()
-            else -> 0
-        }
+        return redisTemplate.opsForValue().get(key) ?: 0
     }
 }
